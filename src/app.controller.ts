92d97ffe7bb7005ccbcd2eb3d7eacdbd15c14848
donghee <coke0701@gmail.com>
2025-06-02 18:18:17 +0900

    feat: 기본 예제 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  userService: any;
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/users')
+  getAll() {
+    return this.appService.findAll();
+  }
+
+  @Get('/user/:id')
+  getOne(@Param('id') id) {
+    return this.appService.findOne(id);
+  }
+
+  @Post('/user')
+  create(@Body() body) {
+    return this.appService.create(body);
+  }
+
+  @Put('/user/:id')
+  update(@Param('id') id, @Body() body) {
+    return this.appService.update(id, body);
+  }
+
+  @Delete('/user/:id')
+  remove(@Param('id') id) {
+    return this.appService.remove(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,47 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  users: { id: number; name: string; }[];
   getHello(): string {
     return 'Hello World!';
+  }
+
+  constructor() {
+    // 임시 데이터
+    this.users = [
+      { id: 1, name: 'Alice' },
+      { id: 2, name: 'Bob' },
+    ];
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: string) {
+    return this.users.find(user => user.id === parseInt(id));
+  }
+
+  create(user: any) {
+    const newUser = { id: Date.now(), ...user };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: any, updateData: any) {
+    const user = this.findOne(id);
+    if (user) {
+      Object.assign(user, updateData);
+      return user;
+    }
+    return null;
+  }
+
+  remove(id: string) {
+    const index = this.users.findIndex(user => user.id === parseInt(id));
+    if (index !== -1) {
+      return this.users.splice(index, 1)[0];
+    }
+    return null;
   }
 }
